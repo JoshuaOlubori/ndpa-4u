4cfb610e1e23356266c1e478b4502a094455bdf4
JoshuaOlubori <joshuaolubori@gmail.com>
2026-07-06 13:44:33 +0100

    adding app.config.ts to see if it fixes production issue of showing the old AI button

---
+++ app/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  assistant: {
+    // Show the floating input on documentation pages
+    floatingInput: false,
+
+    // Show the "Explain with AI" button in the sidebar
+    explainWithAi: false,
+
+    // Keyboard shortcuts
+    shortcuts: {
+      focusInput: 'meta_i'
+    },
+
+  }
+})
